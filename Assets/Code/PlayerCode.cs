59572cf84dda5f13232fd12a9aea0520e34ee68b
junhkim20 <jk7048@nyu.edu>
2022-10-04 19:22:25 -0400

    Added player code (raycast) and mesh to level 1

--- Assets/Code/PlayerCode.cs
+++ Assets/Code/PlayerCode.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerCode : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    NavMeshAgent _agent; 
+
+    Camera mainCam; 
+
     void Start()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
+        mainCam = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // left click
+        if(Input.GetMouseButton(0)){
+             RaycastHit hit; 
+  
+            if(Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 200)){
+                _agent.SetDestination(hit.point); 
+            }
+        }
         
     }
 }
